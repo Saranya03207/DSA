76329661d0516982c44df818f37e559ed4b58955
saranya <saranyapalanisamy207@gmail.com>
2026-09-18 15:31:15 +0530

    Time: 8 ms (10.51%) | Memory: 51.5 MB (85.59%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,29 +1,31 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int n=grid.length;
-        int m=grid[0].length;
-        int[][] d={{0,1},{1,0},{-1,0},{0,-1}};
-        int cnt=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]=='1'){
-                    cnt++;
-                    Queue<int[]> q=new LinkedList<>();
-                      q.offer(new int[]{i,j});
-                      while(!q.isEmpty()){
-                        int[] u=q.poll();
-                        for(int k=0;k<4;k++){
-                            int nr=u[0]+d[k][0];
-                            int nc=u[1]+d[k][1];
-                            if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]=='1'){
-                                q.offer(new int[]{nr,nc});
-                                grid[nr][nc]=0;
-                            }
+     int[][] dis={{0,1},{1,0},{-1,0},{0,-1}};
+     int cnt=0;
+
+     Queue<int[]>  q=new LinkedList<>();
+     
+     for(int i=0;i<grid.length;i++){
+        for(int j=0;j<grid[0].length;j++){
+            if(grid[i][j]=='1'){
+                cnt++;
+                grid[i][j]='0';
+                q.offer(new int[]{i,j});
+                while(!q.isEmpty()){
+                    int[] a=q.poll();
+                    for(int k=0;k<4;k++){
+                        int nr=a[0]+dis[k][0];
+                        int nc=a[1]+dis[k][1];
+                        if(nr>=0 && nr<grid.length && nc>=0 && nc<grid[0].length && grid[nr][nc]=='1'){
+                            grid[nr][nc]='0';
+                            q.offer(new int[]{nr,nc});
                         }
-                      }
+                    }
                 }
             }
         }
-        return cnt; 
+    } 
+     return cnt;
+     
     }
 }
